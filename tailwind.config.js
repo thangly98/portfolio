9352db3a9cf0e7c4824d45617f4a425c83e8c59c
Finn <thangly.dev@gmail.com>
2024-05-30 15:37:57 +0700

    [update](tailwind css) add hoverOnlyWhenSupported

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       colors: { primary: '#ffb400' },
