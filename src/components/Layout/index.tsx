f239724cbd3d4c72bf3024f6ae5c47ee9594019a
Finn <thangly.dev@gmail.com>
2025-10-01 11:05:58 +0700

    [add] Speed Insights

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 import Particles from '@components/Fragments/Particles';
 import Menu from '@components/Layout/Menu';
 
@@ -8,6 +10,8 @@ function Layout() {
     <>
       <Menu />
       <Outlet />
+
+      <SpeedInsights />
 
       <div className='fixed left-0 top-0 -z-10 h-full w-full'>
         <Particles particleCount={300} particleBaseSize={90} />
