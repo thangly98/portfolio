b51e44e201bf3fac90857da4b76983646afc2cc1
Finn <thangly.dev@gmail.com>
2025-10-01 10:58:46 +0700

    [fix] err build

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,18 @@
-import useFetchApi from '@hooks/useFetchApi'
-import { ILink } from '@interfaces/data'
-import LoadingPage from '@components/Fragments/LoadingPage'
-import ContactPage from '@components/Pages/Contact'
+import { ILink } from '@interfaces';
 
-type IInformation = { email: string; phone: string }
-type IDataContactPage = { information: IInformation; links: ILink[] }
+import LoadingPage from '@components/Fragments/LoadingPage';
+import ContactPage from '@components/Pages/Contact';
+import useFetchApi from '@hooks/useFetchApi';
+
+type IInformation = { email: string; phone: string };
+type IDataContactPage = { information: IInformation; links: ILink[] };
 
 function Contact() {
   const { loading, data } = useFetchApi<Pick<IDataContactPage, 'links'> & { information: [IInformation] }>(
     'https://thangly.hasura.app/api/rest/contact-page'
-  )
-  return <LoadingPage component={data ? <ContactPage data={{ ...data, information: data.information[0] }} /> : null} loading={loading} />
+  );
+  return <LoadingPage component={data ? <ContactPage data={{ ...data, information: data.information[0] }} /> : null} loading={loading} />;
 }
 
-export type { IDataContactPage }
-export default Contact
+export type { IDataContactPage };
+export default Contact;
